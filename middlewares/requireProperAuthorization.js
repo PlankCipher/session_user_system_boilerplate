b146c913b1b069c125a0866bff4a1cb29a36b210
PlankCipher <plankcipher1@protonmail.com>
2021-02-04 01:05:48 +0200

    Fix a typo

--- middlewares/requireProperAuthorization.js
+++ middlewares/requireProperAuthorization.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  let { username } = req.params;
+  const { username } = req.params;
   if (username !== req.session.user.username) {
     const err = new Error('Unauthorized.');
     err.statusCode = 401;
